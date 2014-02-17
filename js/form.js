b911168b89a08a7bfab8faafab333c63035d1774
Negaihoshi <e222et@gmail.com>
2014-02-17 12:09:04 +0800

    add jqeury on event bind

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 $(document).load(function(){
 	$('ui.vk.button').click(function(){
-		        $('.addOptionZone').append('<input type="text" name="">');
+		$('.addOptionZone').append('<input type="text" name="">');
 	});
 });
 $(document).ready(function(){
@@ -32,29 +32,30 @@ $(document).ready(function(){
 
             var title = document.getElementById('title');
             title.onkeyup = title.onkeypress = function () {
-            	document.getElementById('displayTitle').innerHTML = this.value.replace(/\n/g,"<br />").replace("<","&lt;").replace(">","&gt;").replace("\"","&quot;");
+            	document.getElementById('displayTitle').innerHTML = this.value.replace(/\n/g,"<br />").replace("<","&lt;").replace(">","&gt;");
             }
             var titleTextarea = document.getElementById('titleTextarea');
             titleTextarea.onkeyup = titleTextarea.onkeypress = function () {
-            	document.getElementById('displayTexArea').innerHTML = this.value.replace(/\n/g,"<br />").replace("<","&lt;").replace(">","&gt;").replace("\"","&quot;");
+            	document.getElementById('displayTexArea').innerHTML = this.value.replace(/\n/g,"<br />").replace("<","&lt;").replace(">","&gt;");
             }
 
-            $('ui.vk.button').click(function(){
-		        $('.addOptionZone').append('<input type="text" name="">');
-		    });
-
+            $(document).on('click', '.ui.vk.button', function(event){
+                
+                $('.addOptionZone').append('<input type="text" name="">');
+            });
             $('#nextText').click(function(){
                 questCount++;
-                $('#origin').append('<div class="ui two column relaxed grid"><div class="column"><div class="ui segment"><form class="ui form"><div class="ui form"><label>第  題</label><textarea placeholder="問卷說明" id="titleTextarea" name="surveyDetail" style="overflow:hidden"></textarea></div><div class="ui form"><div class="field"><label>答題方式</label><p>單選</p></div></div></form></div></div><div class="column" ><div class="ui segment" id="origin2"><div class="ui orange inverted segment"><h2 id="displayTitle" class="ui center aligned  orange inverted segment"></h2><label>說明</label><p id="displayTexArea"></p><div class="ui input"><input type="text" name="" id="" disabled></div></div></div></div></div>');
+                $('#origin').append('<div class="ui two column relaxed grid"><div class="column"><div class="ui segment"><form class="ui form"><div class="ui form"><label>第 '+ questCount + ' 題</label><textarea placeholder="問卷說明" id="titleTextarea" name="surveyDetail" style="overflow:hidden"></textarea></div><div class="ui form"><div class="field"><label>答題方式</label><p>單選</p></div></div></form></div></div><div class="column" ><div class="ui segment" id="origin2"><div class="ui orange inverted segment"><h2 id="displayTitle" class="ui center aligned  orange inverted segment"></h2><label>說明</label><p id="displayTexArea"></p><div class="ui input"><input type="text" name="" id="" disabled></div></div></div></div></div>');
 
             });
             $('#nextTextArea').click(function(){
                 questCount++;
-                $('#origin').append('<div class="ui two column relaxed grid"><div class="column"><div class="ui segment"><form class="ui form"><div class="ui form"><label>第  題</label><textarea placeholder="問卷說明" id="titleTextarea" name="surveyDetail" style="overflow:hidden"></textarea></div><div class="ui form"><div class="field"><label>答題方式</label><p>單選</p></div></div></form></div></div><div class="column" ><div class="ui segment" id="origin2"><div class="ui orange inverted segment"><h2 id="displayTitle" class="ui center aligned  orange inverted segment"></h2><label>說明</label><p id="displayTexArea"></p><input type="text" name="" id="" disabled></div></div></div></div>');
+                $('#origin').append('<div class="ui two column relaxed grid"><div class="column"><div class="ui segment"><form class="ui form"><div class="ui form"><label>第'+ questCount + '題</label><textarea placeholder="問卷說明" id="titleTextarea" name="surveyDetail" style="overflow:hidden"></textarea></div><div class="ui form"><div class="field"><label>答題方式</label><p>多選</p></div></div></form></div></div><div class="column" ><div class="ui segment" id="origin2"><div class="ui orange inverted segment"><h2 id="displayTitle" class="ui center aligned  orange inverted segment"></h2><label>說明</label><p id="displayTexArea"></p><input type="text" name="" id="" disabled></div></div></div></div>');
             });
             $('#nextRadioBtn').click(function(){
                 questCount++;
-                $('#origin').append('<div class="ui two column relaxed grid"><div class="column"><div class="ui segment"><form class="ui form"><div class="ui form"><label>第 '+ questCount + ' 題</label><textarea placeholder="問卷說明" id="titleTextarea" name="surveyDetail" style="overflow:hidden"></textarea></div><div class="ui form"><div class="field"><div class="addOptionZone"><label>選項名稱</label><input type="button" value="添加選項"  class="ui vk button"><input type="text" name=""></div></div></div></form></div></div><div class="column" ><div class="ui segment" id="origin2"><div class="ui orange inverted segment"><h2 id="displayTitle" class="ui center aligned  orange inverted segment"></h2><label>說明</label><p id="displayTexArea"></p></div></div></div></div>');
+                var optionCount = 1;
+                $('#origin').append('<div class="ui two column relaxed grid"><div class="column"><div class="ui segment"><form class="ui form"><div class="ui form"><label>第 '+ questCount + ' 題</label><textarea placeholder="問卷說明" id="titleTextarea" name="surveyDetail" style="overflow:hidden"></textarea></div><div class="ui form"><div class="field"><div class="addOptionZone"><label>選項名稱</label><input type="button" value="添加選項"  class="ui vk button"><label for="">'+ optionCount +'</label><input type="text" name=""></div></div></div></form></div></div><div class="column" ><div class="ui segment" id="origin2"><div class="ui orange inverted segment"><h2 id="displayTitle" class="ui center aligned  orange inverted segment"></h2><label>說明</label><p id="displayTexArea"></p></div></div></div></div>');
             });
             $('#nextCheck').click(function(){
                 questCount++;
